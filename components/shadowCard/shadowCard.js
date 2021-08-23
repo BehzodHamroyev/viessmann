ce9c9c0b4c4eecf7c9c2c254244cabdc072c3f11
BehzodHamroyev <behzodhamroyev007@gmail.com>
2021-08-23 10:52:32 +0500

    563

--- components/shadowCard/shadowCard.js
+++ components/shadowCard/shadowCard.js
@@ -8,7 +8,7 @@ import Fade from 'react-reveal/Fade';
         <div className="title fw-bold fs-4 my-2">{props.data.title}</div>
         <div className="subTitle text-danger">
           {props.data.icon}
-          <a className="" href={`${props.data.link}`} className="ms-2">
+          <a href={`${props.data.link}`} className="ms-2">
             {`${props.data.linkTitle}`}
           </a>
         </div>
